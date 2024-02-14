64d1461be9b8de0695894d0901e82689e1381450
Chris <dev.chrismachado@gmail.com>
2024-02-14 15:04:22 -0500

    adding orders feature

--- models/order.js
+++ models/order.js
@@ -1,8 +1,85 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-    country: String,
-    user: String,
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
+        required: true
+    }],
+    
+    shippingAddress1: {
+        type: String,
+        required: true
+    },
+    shippingAddress2: {
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    zip: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    status:{
+        type: String,
+        required: true,
+        default: 'Pending'
+    },
+    totalPrice: {
+        type: Number
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    dateOrdered: {
+        type: Date,
+        default: Date.now
+    }
+})
+orderSchema.virtual('id').get(function (){
+    return this._id.toHexString();
 })
 
+orderSchema.set('toJSON',{
+    virtuals: true,
+})
+
+
 exports.Order = mongoose.model('Order', orderSchema);
+
+
+/**
+order example: 
+{
+    "orderItems": [
+        { 
+            "quantity": 3,
+            "product": "65c3a8fcd2d5bffe73706d20"
+        },
+        { 
+            "quantity": 2,
+            "product": "65c4e9dce085464a8f2fae5a"
+        }
+    ],
+    "shippingAddress1": "Street 1",
+    "shippingAddress2": "S-222",
+    "city": "Med",
+    "zip": "00000",
+    "country": "Bat",
+    "phone": "+561231123135",
+    "user": "65c539c9cac826042b5bd5dc"
+}
+ */
--- routers/orders.js
+++ routers/orders.js
@@ -1,17 +1,152 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const { Order } = require('../models/order');
+const { Order } = require('../models/order')
+const { OrderItem } = require('../models/order-item')
 
 router.get('/', async (req, res) => {
-    const orders = await Order.find();
-    if (!orders) {
+    const orderList = await Order.find()
+        .populate('user', 'name')
+        .populate('user', 'name')
+        .populate({
+            path: 'orderItems',
+            populate: { path: 'product', populate: 'category' },
+        })
+        .sort({ dateOrdered: -1 })
+    if (!orderList) {
         res.status(500).json({
-            success:false
+            success: false,
         })
     }
-    res.status(200).json(orders);
-
+    res.status(200).json(orderList)
 })
 
-module.exports = router;
+router.get('/:id', async (req, res) => {
+    const order = await Order.findById(req.params.id)
+        .populate('user', 'name')
+        .populate({
+            path: 'orderItems',
+            populate: { path: 'product', populate: 'category' },
+        })
+    if (!order) {
+        res.status(500).json({
+            success: false,
+        })
+    }
+    res.status(200).json(order)
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const {
+            orderItems,
+            shippingAddress1,
+            shippingAddress2,
+            city,
+            zip,
+            country,
+            phone,
+            status,
+            user,
+        } = req.body
+
+        const orderItmesIds = Promise.all(
+            orderItems.map(async (orderItem) => {
+                let newOrderItem = new OrderItem({
+                    quantity: orderItem.quantity,
+                    product: orderItem.product,
+                })
+                newOrderItem = await newOrderItem.save()
+                return newOrderItem.id
+            })
+        )
+
+        const orderItmesIdsResolved = await orderItmesIds;
+
+        const totalPrices = Promise.all(orderItmesIdsResolved.map(async orderItemId => {
+            const orderItem = await OrderItem.findById(orderItemId).populate('product', 'price'); 
+            const totalPrice = orderItem.product.price * orderItem.quantity;
+            return totalPrice;
+        }))
+
+        const totalPricesResolved = (await totalPrices).reduce((acc,item) => acc + item, 0);
+
+        let order = new Order({
+            orderItems: orderItmesIdsResolved,
+            shippingAddress1,
+            shippingAddress2,
+            city,
+            zip,
+            country,
+            phone,
+            status,
+            totalPrice: totalPricesResolved,
+            user,
+        })
+        order = await order.save()
+        if (!order) {
+            return res.status(404).send('The order cannot be created')
+        }
+        res.status(200).json(order)
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            error: err,
+        })
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const order = await Order.findByIdAndUpdate(
+            req.params.id,
+            {
+                status: req.body.status,
+            },
+            { new: true }
+        )
+        if (!order) {
+            return res.status(404).json({
+                success: false,
+                message: 'The order cannot be updated',
+            })
+        }
+        return res.status(200).json(order)
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            error: err
+        })
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    Order.findByIdAndDelete(req.params.id)
+        .then( async (order) => {
+            if (order) {
+                await order.orderItems.map(async orderItem => {
+                    await OrderItem.findByIdAndDelete(orderItem._id)
+                })
+                
+                return res.status(200).json({
+                    success: true,
+                    message: 'The order is deleted',
+                })
+            } else {
+                return res.status(404).json({
+                    success: false,
+                    message: 'order not found',
+                })
+            }
+        })
+        .catch((err) => {
+            return res.status(400).json({
+                success: false,
+                error: err,
+            })
+        })
+})
+
+
+
+module.exports = router
